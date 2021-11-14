1f4aed62a7d99f00f6dd84a530868decab547508
Imran <mahmud.bubt.150@gmail.com>
2021-11-14 19:50:17 +0600

    Update Nav & File

--- add-phone.php
+++ add-phone.php
@@ -31,7 +31,7 @@
             $photo_data = $data['file_name'];
             if ( $data['status'] == 'yes' ) {
 
-                $sql = "INSERT INTO phone (phone_name, 	phone_brand, phone_os, phone_screen, phone_res, phone_ram, phone_rom, phone_cam_primary, phone_cam_secondary, phone_battery, phone_img,phone_price,phone_processor) values ('$phone_name','$phone_brand','$phone_os','$phone_screen', '$phone_res', '$phone_ram', '$phone_rom', '$phone_cam_primary', '$phone_cam_secondary', '$phone_battery', '$photo_data','$phone_price','$phone_processor')";
+                $sql = "INSERT INTO phone (phone_name,  phone_brand, phone_os, phone_screen, phone_res, phone_ram, phone_rom, phone_cam_primary, phone_cam_secondary, phone_battery, phone_img,phone_price,phone_processor) values ('$phone_name','$phone_brand','$phone_os','$phone_screen', '$phone_res', '$phone_ram', '$phone_rom', '$phone_cam_primary', '$phone_cam_secondary', '$phone_battery', '$photo_data','$phone_price','$phone_processor')";
                 $conn -> query($sql);
                set_msg('Successfully Published');
                header("location: add-phone.php");
@@ -54,47 +54,7 @@
     <!-- Sidebar Start-->
     <div class="d-flex" id="wrapper">
         <!-- Sidebar -->
-        <div class="bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading text-center py-4 main-logo fs-4 fw-bold text-uppercase border-bottom">
-                <i class="fas fa-mobile me-2"></i>Mobo Pedia</div>
-
-            <div class="list-group list-group-flush my-3">
-                <a href="./index.php" class="list-group-item list-group-item-action bg-transparent second-text active">
-                    <i class="fas fa-tachometer-alt me-2"></i>Dashboard
-                </a>
-                <a href="./analytics.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-chart-line me-2"></i>Analytics
-                </a>
-                <div class="dropdown">
-                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="far fa-list-alt me-2"></i>Post
-                </a>
-                    <ul class="dropdown-menu post-dropdown" aria-labelledby="dropdownMenuButton1">
-                      <li><a class="dropdown-item" href="./add-post.php">Add Post</a></li>
-                      <li><a class="dropdown-item" href="./all-post.php">All Post</a></li>
-                      <li><a class="dropdown-item" href="./post-category.php">Category</a></li>
-                    </ul>
-                  </div>
-                  <div class="dropdown">
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="far fa-list-alt me-2"></i>Phone
-                    </a>
-                        <ul class="dropdown-menu post-dropdown" aria-labelledby="dropdownMenuButton2">
-                          <li><a class="dropdown-item" href="./add-phone.php">Add Phone</a></li>
-                          <li><a class="dropdown-item" href="./all-phones.php">All Phones</a></li>
-                        </ul>
-                      </div>
-                    <a href="./brand.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                        <i class="fas fa-tags me-2"></i>Brand
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                        <i class="fas fa-cog me-2"></i>Settings
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold">
-                        <i class="fas fa-power-off me-2"></i>Logout
-                    </a>
-            </div>
-        </div>
+        <?php include './nav.php' ?>
         <!-- Sidebar End-->
 
         <!-- Page Content -->
@@ -223,7 +183,7 @@
                 </form>
             <!-- Main Content End -->
 
-
+</div>
     <?php include './footer.php'; ?>
 </body>
 
--- all-post.php
+++ all-post.php
@@ -18,47 +18,7 @@
      <!-- Sidebar Start-->
      <div class="d-flex" id="wrapper">
         <!-- Sidebar -->
-        <div class="bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading text-center py-4 main-logo fs-4 fw-bold text-uppercase border-bottom">
-                <i class="fas fa-mobile me-2"></i>Mobo Pedia</div>
-
-            <div class="list-group list-group-flush my-3">
-                <a href="./index.php" class="list-group-item list-group-item-action bg-transparent second-text active">
-                    <i class="fas fa-tachometer-alt me-2"></i>Dashboard
-                </a>
-                <a href="./analytics.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                    <i class="fas fa-chart-line me-2"></i>Analytics
-                </a>
-                <div class="dropdown">
-                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="far fa-list-alt me-2"></i>Post
-                </a>
-                    <ul class="dropdown-menu post-dropdown" aria-labelledby="dropdownMenuButton1">
-                      <li><a class="dropdown-item" href="./add-post.php">Add Post</a></li>
-                      <li><a class="dropdown-item" href="./all-post.php">All Post</a></li>
-                      <li><a class="dropdown-item" href="./post-category.php">Category</a></li>
-                    </ul>
-                  </div>
-                  <div class="dropdown">
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="far fa-list-alt me-2"></i>Phone
-                    </a>
-                        <ul class="dropdown-menu post-dropdown" aria-labelledby="dropdownMenuButton2">
-                          <li><a class="dropdown-item" href="./add-phone.php">Add Phone</a></li>
-                          <li><a class="dropdown-item" href="./all-phones.php">All Phones</a></li>
-                        </ul>
-                      </div>
-                    <a href="./brand.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                        <i class="fas fa-tags me-2"></i>Brand
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-                        <i class="fas fa-cog me-2"></i>Settings
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold">
-                        <i class="fas fa-power-off me-2"></i>Logout
-                    </a>
-            </div>
-        </div>
+        <?php include './nav.php' ?>
         <!-- Sidebar End-->
 
         <!-- Page Content -->
@@ -136,7 +96,8 @@
                   </table>
             </div>
             <!-- Main Content End -->
-    
+    </div>
+</div>
             <?php include './footer.php'; ?>
     <script>	
         $('a#data_delete').click(function(){
