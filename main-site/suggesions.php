1b01ba9bcd76e6ed80e93332a3fec9e1802abd6b
Imran <mahmud.bubt.150@gmail.com>
2021-11-18 20:14:37 +0600

    Update Suggested Page

--- main-site/suggesions.php
+++ main-site/suggesions.php
@@ -64,88 +64,6 @@ include './db/function.php';
           </div>
       </div>
 
-      <!-- <div class="column">
-          <div class="select is-primary">
-            <select name="ram">
-              <option>Select Ram</option>
-              <option value="2">2 GB</option>
-              <option value="3">3 GB</option>
-              <option value="4">4 GB</option>
-              <option value="6">6 GB</option>
-              <option value="8">8 GB</option>
-              <option value="16">16 GB</option>
-            </select>
-          </div>
-      </div>
-
-      <div class="column">
-          <div class="select is-primary">
-            <select name="rom">
-              <option>Select Rom</option>
-              <option value="16">16 GB</option>
-              <option value="32">32 GB</option>
-              <option value="64">64 GB</option>
-              <option value="128">128 GB</option>
-              <option value="512">512 GB</option>
-            </select>
-          </div>
-      </div>
-
-      <div class="column">
-          <div class="select is-primary">
-            <select name="primary_cam">
-              <option>Primary Camera</option>
-              <option value="5">5 MP</option>
-              <option value="8">8 MP</option>
-              <option value="10">10 MP</option>
-              <option value="12">12 MP</option>
-              <option value="13">13 MP</option>
-              <option value="14">14 MP</option>
-              <option value="15">15 MP</option>
-              <option value="16">16 MP</option>
-              <option value="20">20 MP</option>
-              <option value="32">32 MP</option>
-              <option value="64">64 MP</option>
-              <option>120 MP</option>
-            </select>
-          </div>
-      </div>
-
-      <div class="column">
-          <div class="select is-primary">
-            <select name="secondary_cam">
-              <option>Secondary Camera</option>
-              <option value="2">2 MP</option>
-              <option value="4">4 MP</option>
-              <option value="5">5 MP</option>
-              <option value="8">8 MP</option>
-              <option value="10">10 MP</option>
-              <option value="12">12 MP</option>
-              <option value="14">14 MP</option>
-              <option value="16">16 MP</option>
-              <option value="20">20 MP</option>
-            </select>
-          </div>
-      </div>
-
-      <div class="column">
-          <div class="select is-primary">
-            <select name="battery">
-              <option>Battery Capecity</option>
-              <option value="2400">2400mAh</option>
-              <option value="3200">3200mAh</option>
-              <option value="3300">3300mAh</option>
-              <option value="3800">3800mAh</option>
-              <option value="4000">4000mAh</option>
-              <option value="4200">4200mAh</option>
-              <option value="4500">4500mAh</option>
-              <option value="4800">4800mAh</option>
-              <option value="5000">5000mAh</option>
-              <option value="6000">6000mAh</option>
-            </select>
-          </div>
-      </div> -->
-
      <div class="column is-1  pricing-divider">
         <p>BDT</p>
       </div>
@@ -159,7 +77,7 @@ include './db/function.php';
         <input class="input is-primary"  type="number" name="price_range_2">
       </div>
       <div class="column is-2">
-        <input  class="button is-primary"  type="submit" value="submit" name="suggesions_submit">
+        <input  class="button is-primary"  type="submit" value="Check" name="suggesions_submit">
       </div>
 
 
@@ -167,27 +85,37 @@ include './db/function.php';
 
 
     </div>
-    <!-- <div class="columns my-2 ">
-      <div class="column is-1  pricing-divider">
-        <p>BDT</p>
-      </div>
-      <div class="column is-2">
-        <input class="input is-primary"  type="number" name="price_range_1">
-      </div>
-      <div class="column is-1 pricing-divider">
-        <p>To</p>
-      </div>
-      <div class="column is-2">
-        <input class="input is-primary"  type="number" name="price_range_2">
-      </div>
-      <div class="column is-2">
-        <input  class="button is-primary"  type="submit" value="submit" name="suggesions_submit">
-      </div>
- 
-    </div> -->
   </form>
   </div>
   <hr>
+
+  <div class="phone-details">
+    
+<?php 
+
+            if (isset($_POST['suggesions_submit'])) {
+              $brand = $_POST['brand'];
+              $processor = $_POST['processor'];
+              $price_input_1 = $_POST['price_range_1'];
+              $price_input_2 = $_POST['price_range_2'];
+
+
+              ?>
+
+              <p><strong>Phone Brand : </strong> <?php 
+              $phone_brand = "SELECT * FROM brand WHERE id = '$brand'";
+              $phone_brand_data = $conn -> query($phone_brand);
+              $fetch_phone_brand_data = $phone_brand_data -> fetch_assoc();
+              echo $fetch_phone_brand_data['brand_name'];
+              ?> > 
+              <strong> Phone Soc : </strong> <?php echo $processor ?> > <strong> Phone Price Range : </strong> <?php echo $price_input_2 ?> To  <?php echo $price_input_2 ?></p>
+              <?php            }
+  
+  ?>
+  
+  </div>
+  
+   
 </section>
 
 <section class="fourth container">
@@ -205,11 +133,6 @@ include './db/function.php';
             if (isset($_POST['suggesions_submit'])) {
               $brand = $_POST['brand'];
               $processor = $_POST['processor'];
-              // $ram = $_POST['ram'];
-              // $rom = $_POST['rom'];
-              // $primary_cam = $_POST['primary_cam'];
-              // $secondary_cam = $_POST['secondary_cam'];
-              // $battery = $_POST['battery'];
               $price_input_1 = $_POST['price_range_1'];
               $price_input_2 = $_POST['price_range_2'];
 
